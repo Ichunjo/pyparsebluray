574d0a519e0c72c99cfd71a0889cee7012ed0832
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-06-05 01:40:02 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+
+NAME = 'pyparsebluray'
+VERSION = '0.1.0'
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author='Vardë',
+    author_email='ichunjo.le.terrible@gmail.com',
+    description='Parse and extract binary data from bluray files',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['pyparsebluray'],
+    url='https://github.com/Ichunjo/pyparsebluray',
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
